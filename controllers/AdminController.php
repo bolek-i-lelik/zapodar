eb3b0a3ccbc4a7b924f3096e86af6fdc8976d45b
seblex <burovav84@mail.ru>
2016-10-02 17:36:44 +0300

    Layout admin.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -4,6 +4,9 @@ namespace app\controllers;
 
 class AdminController extends \yii\web\Controller
 {
+
+	public $layout = 'admin';
+
     public function actionIndex()
     {
         return $this->render('index');
