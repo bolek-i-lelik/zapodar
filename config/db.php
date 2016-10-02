aa7cb1ce182322a242dbeeeb493df125a3b78621
seblex <burovav84@mail.ru>
2016-10-02 15:07:52 +0300

    Первая загрузка

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=zapodar',
+    'username' => 'root',
+    'password' => '050184',
+    'charset' => 'utf8',
+];
